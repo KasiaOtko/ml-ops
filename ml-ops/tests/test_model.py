c3f1b1257e1280a1bb865ebdef3367bdafa7b2e2
KasiaOtko <katin010@gmail.com>
2022-01-10 13:24:49 +0100

    Test of github actions

--- ml-ops/tests/test_model.py
+++ ml-ops/tests/test_model.py
@@ -36,6 +36,7 @@ def test_on_wrong_shape_to_forward():
     model = MyAwesomeConvolutionalModel(10)
     with pytest.raises(ValueError, match = "Expected input is not a 4D tensor."):
         model(torch.randn(1, 2, 3))
+        
 
 if __name__ == '__main__':
     test_on_wrong_shape_to_forward()
