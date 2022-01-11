e2d11a3a098c8e1fefbd5ecc6a2d40434c15a414
KasiaOtko <katin010@gmail.com>
2022-01-11 11:02:59 +0100

    Flake8 updates

--- src/models/model.py
+++ src/models/model.py
@@ -9,7 +9,7 @@ class MyAwesomeModel(nn.Module):
         self.fc1 = nn.Linear(input_size, hidden_units1)
         self.fc2 = nn.Linear(hidden_units1, hidden_units2)
         self.output = nn.Linear(hidden_units2, output_size)
-        
+
     def forward(self, x):
         x = x.view(x.shape[0], -1)
         x = F.relu(self.fc1(x))
@@ -18,29 +18,30 @@ class MyAwesomeModel(nn.Module):
 
         return x
 
+
 class MyAwesomeConvolutionalModel(nn.Module):
     def __init__(self, output_size):
         super().__init__()
 
         self.num_classes = output_size
-        
+
         # First convolutional layer
-        self.conv_1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=3, stride=1, padding = (1, 1))
+        self.conv_1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=3, stride=1, padding=(1, 1))
         self.batchnorm1 = nn.BatchNorm2d(10)
 
         # Second convolutional layer + pooling
-        self.conv_2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size = 3, stride=1, padding = (1, 1))
+        self.conv_2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=3, stride=1, padding=(1, 1))
         self.batchnorm2 = nn.BatchNorm2d(20)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # 28 -> 14
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)  # 28 -> 14
 
         # Third convolutional layer
-        self.conv_3 = nn.Conv2d(in_channels=20, out_channels=30, kernel_size = 3, stride=1, padding = (1, 1))
+        self.conv_3 = nn.Conv2d(in_channels=20, out_channels=30, kernel_size=3, stride=1, padding=(1, 1))
         self.batchnorm3 = nn.BatchNorm2d(30)
 
         # Thourth convolutional layer + pooling
-        self.conv_4 = nn.Conv2d(in_channels=30, out_channels=30, kernel_size = 3, stride=1, padding = (1, 1))
+        self.conv_4 = nn.Conv2d(in_channels=30, out_channels=30, kernel_size=3, stride=1, padding=(1, 1))
         self.batchnorm4 = nn.BatchNorm2d(30)
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) # 14 -> 7
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)  # 14 -> 7
 
         # Ouput layer
         self.in_features = 7*7*30
